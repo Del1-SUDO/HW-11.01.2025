e24f1287b2799cd438a8f892e9e6096f66de9c84
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-11 13:14:12 +0500

    done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+class Program {
+    static void Main() {
+        int[] arr = new int[6];
+
+        Console.WriteLine("Enter 6 numbers:");
+        for (int i = 0; i < 6; i++) {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        SeparateNumbers(arr);
+    }
+
+    static void SeparateNumbers(int[] arr) {
+        List<int> even = new List<int>();
+        List<int> odd = new List<int>();
+
+        foreach (int number in arr) {
+            if (number % 2 == 0) { 
+                even.Add(number);
+            }
+            else {
+                odd.Add(number);
+            }
+        }
+
+        Console.WriteLine("Четные: " + string.Join(", ", even));
+        Console.WriteLine("Нечетные: " + string.Join(", ", odd));
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program {
+    static void Main()
+    {
+        Console.Write("Enter the first array size: ");
+        int a = int.Parse(Console.ReadLine());
+        int[] arr1 = new int[a];
+        Console.WriteLine("Enter the first array elements: ");
+        for (int i = 0; i < a; i++){
+            arr1[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Enter the second array size: ");
+        int b = int.Parse(Console.ReadLine());
+        int[] arr2 = new int[b];
+        Console.WriteLine("Enter the second array elements: ");
+        for (int i = 0; i < b; i++){
+            arr2[i] = int.Parse(Console.ReadLine());
+        }
+
+        FindCommon(arr1, arr2, a, b);
+    }
+
+    static void FindCommon(int[] arr1, int[] arr2, int a, int b) {
+        int[] result = new int[Math.Min(a, b)];
+        int index = 0;
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if (arr1[i] == arr2[j]) {
+                    bool alreadyAdded = false;
+                    for (int k = 0; k < index; k++) {
+                        if (result[k] == arr1[i]) {
+                            alreadyAdded = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyAdded) {
+                        result[index++] = arr1[i];
+                    }
+                }
+            }
+        }
+
+        Console.Write("[ ");
+        for (int i = 0; i < index; i++) {
+            Console.Write(result[i] + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
